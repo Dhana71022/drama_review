704b48a2693602671e662b488909a280b3dc91b2
Dhana71022 <145017761+Dhana71022@users.noreply.github.com>
2024-12-04 18:38:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+function search(){
+    var query=document.getElementById("search").value.toLowerCase();
+    if(query==="goblin"){
+      window.location.href="Goblin.html";
+    }
+    else if(query==="go ahead"){
+      window.location.href="GoAhead.html";
+    }
+    else if(query==="chinese drama"){
+      window.location.href="chinese.html";
+    }
+    else if(query==="korean drama"){
+      window.location.href="korean.html";
+    }
+    else if(query==="little forest"){
+      window.location.href="little forest.html";
+    }
+    else if(query==="playful kiss"){
+      window.location.href="playful kiss.html";
+    } 
+    else if(query==="lovely runner"){
+      window.location.href="lovely runner.html";
+    } 
+    else if(query==="my demon"){
+      window.location.href="my demon.html";
+    } 
+    else if(query==="movie"){
+      window.location.href="movie.html";
+    } 
+    else if(query==="thai drama"){
+      window.location.href="thai.html";
+    }
+    else if(query==="japan drama"){
+      window.location.href="Japan.html";
+    }
+    else if(query==="falling into your smile"){
+      window.location.href="falling into your smile.html";
+    }
+    else if(query==="our secret"){
+      window.location.href="our secret.html";
+    }
+    else if(query==="crush"){
+      window.location.href="crush.html";
+    }
+    else{
+      alert("Not Found")
+    }
+  }
+  
